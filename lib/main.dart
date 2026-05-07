712256d1f63c254af3fe0edc52943b77fde0621c
Willhelmus Juaness Patudang <willyjuaness@gmail.com>
2026-05-07 12:21:58 +0800

    Fix API Key case sensitivity for Web 2

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ void main() async {
     await Firebase.initializeApp(
       options: kIsWeb
           ? const FirebaseOptions(
-              apiKey: 'AIzaSyBvBsMwkOZbk0ac5ozek55zrzApdQWKL8E',
+              apiKey: 'AIzaSyBXkRJNkoZtyyXi3cJoZ9Z_otkvwbuTJSw',
               appId: '1:214650731637:web:63d02f35accef054000c3c',
               messagingSenderId: '214650731637',
               projectId: 'finalwilly-8b446',
