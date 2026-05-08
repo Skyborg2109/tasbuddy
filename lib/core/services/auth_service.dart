6a3667d9df1a430db1de39a6e82419e86fbf20d7
Willhelmus Juaness Patudang <willyjuaness@gmail.com>
2026-05-08 12:27:52 +0800

    Deploy fungsi login google 2

--- lib/core/services/auth_service.dart
+++ lib/core/services/auth_service.dart
@@ -40,6 +40,10 @@ class AuthService {
   Future<UserCredential?> signInWithGoogle() async {
     try {
       GoogleAuthProvider googleProvider = GoogleAuthProvider();
+      // Force account selection
+      googleProvider.setCustomParameters({
+        'prompt': 'select_account',
+      });
       return await _auth.signInWithPopup(googleProvider);
     } on FirebaseAuthException catch (e) {
       throw _handleAuthException(e);
